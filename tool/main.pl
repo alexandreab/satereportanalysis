ba0e0883e892968a6a3a7578c79873fcccef4f7b
Alexandre Barbosa <alexandreab@live.com>
2014-12-18 13:21:43 -0200

    Main generating all tables

--- tool/main.pl
+++ tool/main.pl
@@ -7,33 +7,54 @@ use Calculator;
 use Output;
 
 my @cwe_ids = ();
-#my $calc = new Calculator("synthetic-c121-122.xml");
 my $calc = new Calculator("synthetic-c.xml");
 
 my $output = new Output();
 
-# {
-#   @cwe_ids = (126, 127);
-#   my (@values, @metrics) = $calc->calculate("Buffer Overflows relacionados a Leitura de Buffer",\@cwe_ids);
-#   print $output->to_latex("Buffer Overflows relacionados a Leitura de Buffer", @table_values);
-#   print "\n\n";
-#   print $output->to_latex("Buffer Overflows relacionados a Leitura de Buffer", @table_metrics);
-#   print "%----------------------------------------------------------------%\n";
-# }
-# 
-# {
-#   @cwe_ids = (124);
-#   my (@values, @metrics) = $calc->calculate("Buffer Overflows relacionados a Escrita de Buffer",\@cwe_ids);
-#   print "%----------------------------------------------------------------%\n";
-# }
+{
+  @cwe_ids = (126,127);
+  my ($values_ref, $metrics_ref) = $calc->calculate(@cwe_ids);
+  my @values = @$values_ref;
+  my @metrics = @$metrics_ref;
+  print $output->to_latex("Buffer Overflows relacionados a Leitura de Buffer - Valores",@values);
+  print "\n\n";
+  print $output->to_latex("Buffer Overflows relacionados a Leitura de Buffer - Metricas",@metrics);
+  print "%----------------------------------------------------------------%\n";
+}
 
-# @cwe_ids = (124, 127);
-# $calc->calculate("Buffer Overflows relacionados ao limite inicial do Buffer",\@cwe_ids);
-# print "%----------------------------------------------------------------%\n";
-# 
-# @cwe_ids = (126);
-# $calc->calculate("Buffer Overflows relacionados ao limite final do Buffer",\@cwe_ids);
-# print "%----------------------------------------------------------------%\n";
+{
+  @cwe_ids = (124);
+  my ($values_ref, $metrics_ref) = $calc->calculate(@cwe_ids);
+  my @values = @$values_ref;
+  my @metrics = @$metrics_ref;
+  print $output->to_latex("Buffer Overflows relacionados a Escrita de Buffer - Valores",@values);
+  print "\n\n";
+  print $output->to_latex("Buffer Overflows relacionados a Escrita de Buffer - Metricas",@metrics);
+  print "%----------------------------------------------------------------%\n";
+}
+
+{
+  @cwe_ids = (124,127);
+  my ($values_ref, $metrics_ref) = $calc->calculate(@cwe_ids);
+  my @values = @$values_ref;
+  my @metrics = @$metrics_ref;
+  print $output->to_latex("Buffer Overflows relacionados ao Limite Inicial do Buffer - Valores",@values);
+  print "\n\n";
+  print $output->to_latex("Buffer Overflows relacionados ao Limite Inicial do Buffer - Metricas",@metrics);
+  print "%----------------------------------------------------------------%\n";
+}
+
+{
+  @cwe_ids = (126);
+  my ($values_ref, $metrics_ref) = $calc->calculate(@cwe_ids);
+  my @values = @$values_ref;
+  my @metrics = @$metrics_ref;
+  print $output->to_latex("Buffer Overflows relacionados ao Limite Final do Buffer - Valores",@values);
+  print "\n\n";
+  print $output->to_latex("Buffer Overflows relacionados ao Limite Final do Buffer - Metricas",@metrics);
+  print "%----------------------------------------------------------------%\n";
+}
+
  
 {
   @cwe_ids = (121);
